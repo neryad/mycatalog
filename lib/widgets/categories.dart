f7147365d35b397e09e0813dc2bdd5b301accc2d
neryad <dayern.gomez@hotmail.com>
2020-09-19 20:06:47 -0400

    categories Widtget

---
+++ lib/widgets/categories.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class Categories extends StatefulWidget {
+  Categories({Key key}) : super(key: key);
+
+  @override
+  _CategoriesState createState() => _CategoriesState();
+}
+
+class _CategoriesState extends State<Categories> {
+  List<String> categories = ["Hang bag", "Jewellrty", "Footwear", "Dresses"];
+  int selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 20.0),
+      child: SizedBox(
+        height: 25,
+        child: ListView.builder(
+            scrollDirection: Axis.horizontal,
+            itemCount: categories.length,
+            itemBuilder: (context, index) => buildCategories(index)),
+      ),
+    );
+  }
+
+  Widget buildCategories(int index) {
+    return GestureDetector(
+      onTap: () {
+        setState(() {
+          selectedIndex = index;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              categories[index],
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color:
+                      selectedIndex == index ? Colors.black : Colors.black38),
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 20.0 / 4),
+                height: 2,
+                width: 30,
+                color: selectedIndex == index
+                    ? Colors.black38
+                    : Colors.transparent)
+          ],
+        ),
+      ),
+    );
+  }
+}
