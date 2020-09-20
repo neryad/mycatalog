08160e70d876861ff5f1057605e5e508db8ef388
neryad <dayern.gomez@hotmail.com>
2020-09-19 20:03:26 -0400

    model of products

---
+++ lib/models/ProductsModel.dart
@@ -0,0 +1,41 @@
+import 'dart:convert';
+
+Products productsFromJson(String str) => Products.fromJson(json.decode(str));
+
+String productsToJson(Products data) => json.encode(data.toJson());
+
+class Products {
+  Products({
+    this.id,
+    this.title,
+    this.price,
+    this.description,
+    this.category,
+    this.image,
+  });
+
+  int id;
+  String title;
+  double price;
+  String description;
+  String category;
+  String image;
+
+  factory Products.fromJson(Map<String, dynamic> json) => Products(
+        id: json["id"],
+        title: json["title"],
+        price: json["price"].toDouble(),
+        description: json["description"],
+        category: json["category"],
+        image: json["image"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": category,
+        "image": image,
+      };
+}
